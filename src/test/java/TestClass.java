7fb6f375e566a161fe61c50df65266c818088a5a
Da1nwood <Mexikos2018@gmail.com>
2019-07-09 12:33:09 +0300

    Go

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -4,11 +4,11 @@ public class TestClass {
 
     @Test
     public void test1(){
-        System.out.println("Sound");
+        System.out.println("Sound1");
     }
 
     @Test
     public void test2(){
-        System.out.println("Sound");
+        System.out.println("Sound2");
     }
 }
