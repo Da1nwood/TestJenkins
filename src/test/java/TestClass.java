a7c5798926e2e6f0d374db0816a5eb1782a0407c
Da1nwood <Mexikos2018@gmail.com>
2019-07-09 14:01:40 +0300

    Go3

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,14 +1,43 @@
 import org.testng.annotations.Test;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
 
 public class TestClass {
-
+    private static final String xmlFilePath = "C:\\Users\\user\\Desktop\\Test.xml";
     @Test
     public void test1(){
-        System.out.println("Sound1");
+
     }
 
     @Test
-    public void test2(){
-        System.out.println("Sound2");
-    }
+    public void test2() throws ParserConfigurationException, TransformerException {
+            DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
+            Document document = documentBuilder.newDocument();
+            Element suite = document.createElement("suite");
+            document.appendChild(suite);
+            Attr verbose = document.createAttribute("verbose");
+            verbose.setValue("10");
+            Attr name = document.createAttribute("name");
+            name.setValue(this.getClass().getName());
+            suite.setAttributeNode(verbose);
+            suite.setAttributeNode(name);
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource domSource = new DOMSource(document);
+            StreamResult streamResult = new StreamResult(new File(xmlFilePath));
+            transformer.transform(domSource, streamResult);
+        }
 }
