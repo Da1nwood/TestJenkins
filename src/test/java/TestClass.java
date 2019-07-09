6a3b727f2d79bcabf66447ba66e0cb9acacbfa85
Da1nwood <Mexikos2018@gmail.com>
2019-07-09 12:05:20 +0300

    Go

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+    @Test
+    public void test1(){
+        System.out.println("Sound");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Sound");
+    }
+}
